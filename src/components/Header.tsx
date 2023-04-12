b4adbfde542744527e0217e8b6e327976d69580c
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2023-04-13 01:00:19 +0530

    cleanups

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,4 @@
 import React ,{useEffect, useRef, useState}from "react";
-import { Link
- } from "react-router-dom";
 import logo from "../assets/bg.png";
 const Header = () => {
     const [clicked,setclicked]=useState(false);
