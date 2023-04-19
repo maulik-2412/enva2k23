58d939214809d8d34c8e9575a7be7344c4979299
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2023-04-19 20:06:19 +0530

    fix sponsor

--- src/components/SponsorComponent.tsx
+++ src/components/SponsorComponent.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Sponsor } from "../models/sponsor";
 import { getFilePreview } from "../api/storage";
 import { Constants } from "../utils/constants";
 
 export const SponsorComponent = (prop: { sponsor: Sponsor }) => {
   const [image, setImage] = useState<string>("");
-  async (id: string) => {
+  async function getSponsorImage(id: string) {
     const url = await getFilePreview(Constants.SPONSORHIP_BUCKET, id);
     setImage(url.href);
-  };
+  }
+
+  useEffect(() => {
+    getSponsorImage(prop.sponsor.image_id);
+  }, []);
   return (
     <div className="slide">
       <img src={image} alt={prop.sponsor.sponsor_name} />
--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import { getSponsors } from "../api/database";
-import img from "../assets/images/adidas.png";
 import { Sponsor } from "../models/sponsor";
-import { getFilePreview } from "../api/storage";
-import { Constants } from "../utils/constants";
 import { SponsorComponent } from "../components/SponsorComponent";
 
 function Sponsors() {
@@ -20,7 +17,7 @@ function Sponsors() {
     return (
       <div className="sponsor-page" id="sponsors">
         <div className="sponsor-container">
-          <h2 className="sponsor-heading">Our Partners</h2>
+          <h2 className="sponsor-heading">Sponsors</h2>
           <section className="sponsor-image-container">
             {sponsors.map((sponsor) => (
               <SponsorComponent sponsor={sponsor} />
